2f680e9bd4302ec489fce9ca6c0cd79bf0f48d3d
laxshi24 <laxshisiva@gmail.com>
2025-01-31 12:29:10 +0530

    profile

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -12,7 +12,7 @@ const AppContextProvider = (props) =>{
     const [doctors,setDoctors] = useState([])
 
     const [token,setToken] = useState(localStorage.getItem('token') ?localStorage.getItem('token'):false)
-
+    const [userData,setUserData] = useState(false)
   
 
     const getDoctorsData= async () => {
@@ -31,16 +31,41 @@ const AppContextProvider = (props) =>{
 
     }
 
+    const loadUserProfileData = async() => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/user/get-profile',{headers:{token}})
+            if (data.success) {
+                setUserData(data.userData)
+                
+            }else{
+                toast.error(error.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     const value ={
         doctors,
         currencySymbol,
         token,setToken,
-        backendUrl
+        backendUrl,
+        userData,setUserData,
+        loadUserProfileData
     }
 
     useEffect(()=>{
         getDoctorsData()
     },[])
+
+    useEffect(()=>{
+        if (token) {
+            loadUserProfileData()
+        }else{
+            setUserData(false)
+        }
+    },[token])
 
     return(
         <AppContext.Provider value={value}>
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,26 +1,63 @@
-import React, { useState } from 'react'
-import { assets } from '../assets/assets'
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import {assets} from '../assets/assets'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const MyProfile = () => {
 
-  const [userData,setUserData] = useState({
-    name:"Edward vincent",
-    image:assets.profile_pic,
-    email:'edwin34@gmail.com',
-    phone:'+94 077 345 6789',
-    address:{
-      line1:"43th Cross,malabe",
-      line2:"Colombo12,Sri Lanka",
-    },
-    gender:"Male",
-    dob:'200-01-25'
-  })
+  
+  const {userData,setUserData,token,backendUrl,loadUserProfileData} = useContext(AppContext)
+  
   const [isEdit,setIsEdit] = useState(false)
+  const [image,setImage] = useState(false)
+
+  const updateUserProfileData = async () => {
+
+    try {
+      const formData = new FormData()
+      formData.append('name',userData.name)
+      formData.append('phone',userData.phone)
+      formData.append('address',JSON.stringify(userData.address))
+      formData.append('gender',userData.gender)
+      formData.append('dob',userData.dob)
+
+      image && formData.append('image',image)
+
+      const {data} = await axios.post(backendUrl + '/api/user/update-profile',formData,{headers:{token}})
+      
+      if (data.success) {
+        toast.success(data.message)
+        await loadUserProfileData()
+        setIsEdit(false)
+        setImage(false)
+      }else{
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
+  }
 
 
-  return (
+  return userData &&  (
     <div className='max-w-lg flex flex-col gap-2 text-sm'>
-      <img className='w-36 rounded' src={userData.image} alt="" />
+
+      {
+        isEdit
+        ? <label htmlFor="image">
+              <div className='inline-block relative cursor-pointer'>
+                <img className='w-36 rounded opacity-75' src={image ? URL.createObjectURL(image) : userData.image}  alt="" />
+                <img className='w-10 absolute bottom-12 right-12' src={image ? '' : assets.upload_icon} alt="" />
+                <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden/>
+              </div>
+        </label>
+        :<img className='w-36 rounded' src={userData.image} alt="" />
+      }
+      
 
       {
         isEdit
@@ -86,7 +123,7 @@ const MyProfile = () => {
         <div className='mt-10'>
           {
             isEdit
-            ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=> setIsEdit(false)}>Save Information</button>
+            ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={updateUserProfileData}>Save Information</button>
             : <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=> setIsEdit(true)}>Edit</button>
           }
         </div>
